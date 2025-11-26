5dc14a726896bdd1d584e224a5a32a9279d97282
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-11-26 12:29:20 +0100

    add not-found file

---
+++ app/not-found.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import css from './home.module.css';
+
+export const metadata: Metadata = {
+  title: 'Not-found',
+  description: 'The page you are looking for does not exist.',
+
+  openGraph: {
+    title: 'Not-found',
+    description: 'The page you are looking for does not exist.',
+    url: 'http://localhost:3000/',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: ' Not-found',
+      },
+    ],
+  },
+};
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className={css.title}>404 - Page not found</h1>
+      <p className={css.description}>
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
