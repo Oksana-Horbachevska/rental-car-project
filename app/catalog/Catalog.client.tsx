19a0aad8cb7dfc2d0c094e34da4e1798d2be7e42
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-11-29 16:55:53 +0100

    add filter by brand, price and mileage

--- app/catalog/Catalog.client.tsx
+++ app/catalog/Catalog.client.tsx
@@ -4,8 +4,17 @@ import CarList from '@/components/CarList/CarList';
 import { fetchBrandsClient, fetchCarsClient } from '@/lib/api/clientApi';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import css from './Catalog.module.css';
+import { ChangeEventHandler, useState } from 'react';
 
 const CatalogClient = () => {
+  const [formFilters, setFormFilters] = useState({
+    brand: '',
+    rentalPrice: '',
+    minMileage: '',
+    maxMileage: '',
+  });
+  // --- -------------- Paginate and filter with Infinite Query------------- ---
+  const [appliedFilters, setAppliedFilters] = useState(formFilters);
   const {
     data,
     fetchNextPage,
@@ -13,22 +22,57 @@ const CatalogClient = () => {
     isFetchingNextPage,
     isLoading,
     error,
+    refetch,
   } = useInfiniteQuery({
-    queryKey: ['cars'],
+    queryKey: ['cars', appliedFilters],
     queryFn: async ({ pageParam = 1 }) => {
-      return await fetchCarsClient(pageParam.toString(), '12', '', '', '', '');
+      return await fetchCarsClient(
+        pageParam.toString(),
+        '12',
+        appliedFilters.brand,
+        appliedFilters.rentalPrice,
+        appliedFilters.minMileage,
+        appliedFilters.maxMileage
+      );
     },
     initialPageParam: 1,
     getNextPageParam: lastPage =>
       lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined,
     refetchOnMount: false,
   });
+  // ------------------get brands with Simple Query -----------------
+  const {
+    data: brandsData,
+    isLoading: isBrandsLoading,
+    error: brandsError,
+  } = useQuery({
+    queryKey: ['brands'],
+    queryFn: fetchBrandsClient,
+    staleTime: 1000 * 60 * 60, // cache for 1 hour
+  });
 
-  if (isLoading) return 'Is loading, please wait...';
-  if (error || !data) return 'Error loading catalog';
+  if (isLoading || isBrandsLoading) return 'Is loading, please wait...';
+  if (error || brandsError || !data) return 'Error loading catalog';
 
   const cars = data?.pages?.flatMap(p => p.cars) ?? [];
-  console.log('cars:', cars);
+  const brands = brandsData || [];
+  const uniquePrices = [...new Set(cars.map(car => car.rentalPrice))].filter(
+    price => price !== undefined
+  );
+
+  const handleFilterChange: ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = e => {
+    const { name, value } = e.target;
+
+    setFormFilters(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSearchClick = () => {
+    setAppliedFilters(formFilters);
+  };
 
   return (
     <div className={`container ${css.pageContainer}`}>
@@ -36,7 +80,12 @@ const CatalogClient = () => {
         <label htmlFor="Car_brand" className={css.label}>
           Car brand
           <div className={css.selectWrapper}>
-            <select name="filter" className={css.select}>
+            <select
+              name="brand"
+              className={css.select}
+              onChange={handleFilterChange}
+              value={formFilters.brand}
+            >
               <option
                 value=""
                 disabled
@@ -46,17 +95,23 @@ const CatalogClient = () => {
               >
                 Choose a brand
               </option>
-              <option value="brand1" className={css.option}>
-                brand1
-              </option>
-              <option value="brand2">brand2</option>
+              {brands.map((brand, index) => (
+                <option value={brand} key={index} className={css.option}>
+                  {brand}
+                </option>
+              ))}
             </select>
           </div>
         </label>
         <label htmlFor="Price" className={css.label}>
           Price/ 1 hour
           <div className={css.selectWrapper}>
-            <select name="filter" className={css.select}>
+            <select
+              name="rentalPrice"
+              className={css.select}
+              onChange={handleFilterChange}
+              value={formFilters.rentalPrice}
+            >
               <option
                 value=""
                 disabled
@@ -66,10 +121,15 @@ const CatalogClient = () => {
               >
                 Choose a price
               </option>
-              <option value="brand1" className={css.option}>
-                brand1
-              </option>
-              <option value="brand2">brand2</option>
+              {uniquePrices.map((price, index) => (
+                <option
+                  value={price}
+                  key={price + index}
+                  className={css.option}
+                >
+                  {price}
+                </option>
+              ))}
             </select>
           </div>
         </label>
@@ -80,11 +140,25 @@ const CatalogClient = () => {
               type="text"
               className={css.mileageInput}
               placeholder="From "
+              name="minMileage"
+              onChange={handleFilterChange}
+              value={formFilters.minMileage}
             />
-            <input type="text" className={css.mileageInput} placeholder="To" />
+            <input
+              type="text"
+              className={css.mileageInput}
+              placeholder="To"
+              name="maxMileage"
+              onChange={handleFilterChange}
+              value={formFilters.maxMileage}
+            />
           </div>
         </label>
-        <button type="button" className={css.searchButton}>
+        <button
+          type="button"
+          className={css.searchButton}
+          onClick={handleSearchClick}
+        >
           Search
         </button>
       </div>
